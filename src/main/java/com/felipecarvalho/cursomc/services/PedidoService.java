46804f107cde3013f04783553300268b02e5ea0b
Felipe Carvalho de Souza <desouzafelipecarvalho@gmail.com>
2019-01-14 11:16:57 -0200

    Endopoint /pedidos/{id} disponobilizado

---
+++ src/main/java/com/felipecarvalho/cursomc/services/PedidoService.java
@@ -0,0 +1,26 @@
+package com.felipecarvalho.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipecarvalho.cursomc.domain.Pedido;
+import com.felipecarvalho.cursomc.repositories.PedidoRepository;
+import com.felipecarvalho.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	//essa dependência é automaticamente instanciada pelo spring
+	//graças a anotação autowired
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+		}
+}
