cf1aab9afbb5b08598631a3819a5da2aee500714
Felipe Carvalho de Souza <desouzafelipecarvalho@gmail.com>
2019-01-16 20:09:13 -0200

    Validação sintática com Bean Validation

--- src/main/java/com/felipecarvalho/cursomc/services/CategoriaService.java
+++ src/main/java/com/felipecarvalho/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.felipecarvalho.cursomc.domain.Categoria;
+import com.felipecarvalho.cursomc.dto.CategoriaDTO;
 import com.felipecarvalho.cursomc.repositories.CategoriaRepository;
 import com.felipecarvalho.cursomc.services.exceptions.DataIntegrityException;
 import com.felipecarvalho.cursomc.services.exceptions.ObjectNotFoundException;
@@ -57,5 +58,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);			
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
