3402249bf86071b3f6c165e3721c6358611fc40b
Felipe Carvalho de Souza <desouzafelipecarvalho@gmail.com>
2019-01-20 20:24:44 -0200

    Email HTML

--- src/main/java/com/felipecarvalho/cursomc/services/EmailService.java
+++ src/main/java/com/felipecarvalho/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.felipecarvalho.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.felipecarvalho.cursomc.domain.Pedido;
@@ -9,4 +11,10 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	// versão HTML dos métodos acima
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
