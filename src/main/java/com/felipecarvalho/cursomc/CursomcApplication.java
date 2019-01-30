03583841e3ac9e112bdcc15685b522c86568d7ee
Felipe Carvalho de Souza <desouzafelipecarvalho@gmail.com>
2019-01-30 21:54:24 -0200

    Salvando primeiro arquivo no S3

--- src/main/java/com/felipecarvalho/cursomc/CursomcApplication.java
+++ src/main/java/com/felipecarvalho/cursomc/CursomcApplication.java
@@ -1,8 +1,11 @@
 package com.felipecarvalho.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.felipecarvalho.cursomc.services.S3Service;
 
 	
 // ao implementar CommandLineRunner, é permitido instanciar objetos assim que a aplicação for rodada.
@@ -10,13 +13,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("C:\\Users\\Felipe\\Pictures\\cao-shitzu.jpg");
 	}
 
 }
